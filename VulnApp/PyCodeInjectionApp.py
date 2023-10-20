165466653fdbdd404fdd5e088cced1a91b610195
PranayaSinha <93397150+PranayaSinha@users.noreply.github.com>
2023-10-19 20:12:32 -0700

    Update PyCodeInjectionApp.py

--- VulnApp/PyCodeInjectionApp.py
+++ VulnApp/PyCodeInjectionApp.py
@@ -1,123 +1,87 @@
 #!/usr/bin/env python3
 
 """
- Authors: Seth Art (sethsec@gmail.com, @sethsec), Charlie Worrell (@decidedlygray, https://keybase.io/decidedlygray)
- Purpose: Web application intentinally vulnerable to Python Code Injection  
-          
+Authors: Seth Art (sethsec@gmail.com, @sethsec), Charlie Worrell (@decidedlygray, https://keybase.io/decidedlygray)
+Purpose: Web application intentionally vulnerable to Python Code Injection
+
+WARNING: This application is intentionally vulnerable and should not be used in a production environment or for any actual security purpose. The use of 'eval' is dangerous as it allows code execution.
 """
 
 import web
-import urllib
-import sys, StringIO
+import io
+import sys
 
 
 urls = (
-  '/', 'index',
-  '/pyinject', 'pyinject'
+    '/', 'index',
+    '/pyinject', 'pyinject'
 )
+
 render = web.template.render('templates/')
 app = web.application(urls, globals())
+
 
 class index:
     def GET(self):
         return "Hello World. Go to /pyinject for the fun"
+
     def POST(self):
         return "Hello World. Go to /pyinject for the fun"
 
+
 class pyinject:
-    def POST(self):        
+    def evaluate_code(self, code):
+        """
+        This method is a security risk and is used only for demonstration purposes. 
+        It evaluates the Python code contained in the provided string.
+        """
         eval_output = ''
-        #redir sys.stdout, from https://joecodeswell.wordpress.com/2012/07/28/378/
-        stdout = sys.stdout
-        sys.stdout = reportSIO = StringIO.StringIO()
+        stdout = sys.stdout  # Save the original stdout
+        sys.stdout = reportSIO = io.StringIO()
+
+        try:
+            result = eval(code)  # This is a security risk
+            if result:
+                eval_output = str(result)
+        except Exception as e:
+            print(f"An error occurred: {e}")
+        finally:
+            reportStr = reportSIO.getvalue()
+            sys.stdout = stdout  # Restore the original stdout
+
+            return str(reportStr) + eval_output
+
+    def process_input(self, method):
+        """
+        Process the input based on the HTTP method (POST or GET).
+        """
+        eval_output = ''
         try:
             get_input = web.input()
-            param1 = get_input['param1'] if 'param1' in get_input else None
-            param2 = get_input['param2'] if 'param2' in get_input else None            
-            cookie1 = web.cookies().get('c1') if 'c1' in web.cookies() else None
-            if not cookie1: 
-                web.setcookie('c1','exploit_me', expires="", domain=None, secure=False)             
-            if (param1):
-                try:
-                    x = eval(param1)
-                    if (x):
-                        eval_output = str(eval_output) + x                    
-                except:
-                    pass
-            if (param2):
-                try:
-                    y = eval(param2)
-                    if (y):
-                        eval_output = str(eval_output) + y                    
-                except:
-                    pass
-            if (cookie1):
-                try:
-                    z = eval(str(cookie1))
-                    if (z):
-                        eval_output = str(eval_output) + z 
-                except:
-                    pass
-                                   
-            reportStr = reportSIO.getvalue()
-            #restore sys.stdout so we can print
-            sys.stdout = stdout
-            
-            output = str(reportStr) + str(eval_output)
-            #return output
-            return render.index(output)
-        except Exception as error: 
-            print(error)
+            params = ['param1', 'param2']
+            cookie1 = web.cookies().get('c1')
+
+            if not cookie1:
+                web.setcookie('c1', 'exploit_me', expires="", domain=None, secure=False)
+
+            for param in params:
+                if param in get_input:
+                    eval_output += self.evaluate_code(get_input[param])
+
+            if cookie1:
+                eval_output += self.evaluate_code(str(cookie1))
+
+            return render.index(eval_output)
+        except Exception as error:
+            print(f"An error occurred: {error}")
             return error
+
+    def POST(self):
+        return self.process_input(method='POST')
 
     def GET(self):
-        
-        eval_output = ''
-        #redir sys.stdout, from https://joecodeswell.wordpress.com/2012/07/28/378/
-        stdout = sys.stdout
-        sys.stdout = reportSIO = StringIO.StringIO()
-        try:
-            get_input = web.input()
-            param1 = get_input['param1'] if 'param1' in get_input else None
-            param2 = get_input['param2'] if 'param2' in get_input else None            
-            cookie1 = web.cookies().get('c1') if 'c1' in web.cookies() else None
-            if not cookie1: 
-                web.setcookie('c1','exploit_me', expires="", domain=None, secure=False)     
+        return self.process_input(method='GET')
 
-            if (param1):
-                try:
-                    x = eval(param1)
-                    if (x):
-                        eval_output = str(eval_output) + x                    
-                except:
-                    pass
-            if (param2):
-                try:
-                    y = eval(param2)
-                    if (y):
-                        eval_output = str(eval_output) + y                    
-                except:
-                    pass
-            if (cookie1):
-                try:
-                    z = eval(str(cookie1))
-                    if (z):
-                        eval_output = str(eval_output) + z 
-                except:
-                    pass
-            
-            reportStr = reportSIO.getvalue()
-            #restore sys.stdout so we can print
-            sys.stdout = stdout
-            output = str(reportStr) + str(eval_output)
-            #return output
-            return render.index(output)
-        except Exception as error: 
-            print(error)
-            return error
 
-    
 if __name__ == "__main__":
-    
-    
     app.run()
